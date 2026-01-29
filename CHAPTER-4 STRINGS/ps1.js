281a22ce747686a322aa70569e3adcb069ba4701
th-shivam <dreamyshivam01@gmail.com>
2026-01-29 17:56:33 +0530

    Add Chapter 4 Strings

---
+++ CHAPTER-4 STRINGS/ps1.js
@@ -0,0 +1,13 @@
+// PROBLEM 1 
+// let UserChar = prompt("Enter the word");
+// PrintChar = UserChar.toLowerCase();
+// console.log(PrintChar); 
+
+// PROBLEM 2 
+// let str = " Please give me Rs. 1000"
+// console.log(str.slice(16)) ;
+
+// PROBLEM 4  
+
+let str = "SHIVAM"
+console.log(str.replace("V" , "R"));
